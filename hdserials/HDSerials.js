62b27eb099c5a4ef76f60df32d9e9b99fbd3612c
Wain <wain-pc@mail.ru>
2013-12-15 18:52:04 +0400

    HDSerials 0.7.1.

--- hdserials/HDSerials.js
+++ hdserials/HDSerials.js
@@ -1,7 +1,7 @@
 /**
  *  HDSerials plugin for Showtime
  *
- *  Copyright (C) 2013 Buksa
+ *  Copyright (C) 2013 Buksa, Wain
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,11 +16,12 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-//ver 0.6.9 API
+//ver 0.7.0 API
 (function(plugin) {
     var PREFIX = 'HDSerials';
     var BASE_URL = 'http://hdserials.galanov.net';
     var logo = plugin.path + "img/logo.png";
+    var USER_AGENT = 'Android;HD Serials v.1.7.0;ru-RU;google Nexus 4;SDK 10;v.2.3.3(REL)';
 
     function trim(s) {
         s = s.replace(/(\r\n|\n|\r)/gm, "");
@@ -33,6 +34,8 @@
         return '<font color="6699CC"> (' + str + ')</font>';
     }
 
+    //this MUST be used at the end of the corresponding function
+    //else there is no loading circle
     function setPageHeader(page, title) {
         if (page.metadata) {
             page.metadata.title = PREFIX + ' : ' + title;
@@ -42,6 +45,7 @@
         page.contents = "items";
         page.loading = false;
     }
+    
     var service = plugin.createService("HDSerials.ru", PREFIX + ":start", "video", true, logo);
     var settings = plugin.createSettings("HDSerials", logo, "HDSerials: Integration of the website HDSerials.ru into Showtime");
     settings.createInfo("info", logo, "Plugin developed by Buksa \n");
@@ -52,16 +56,45 @@
         ['2', '480p'],
         ['3', '360p']
     ];
-    settings.createMultiOpt("Resolution", "Resolution", Resolution, function(v) {
+    
+    settings.createBool("Show_finished", "Показывать сообщение о достижении конца директории", true, function(v) {
+        service.showEndOfDirMessage = v;
+    });
+    
+    settings.createMultiOpt("Resolution", "Разрешение", Resolution, function(v) {
         service.Resolution = v;
     });
+    
+    var requestMinDelay = [
+      [1000,'1 сек'],
+      [2000,'2 сек'],
+      [3000,'3 сек',true]
+    ];
+    
+    settings.createMultiOpt("Min.Delay", "Интервал запросов к серверу", requestMinDelay, function(v) {
+        service.requestMinDelay = v;
+    });
+    settings.createInfo("info2",'', "Чем меньше значение - тем быстрее подгрузка списков в директориях с большим количеством файлов, но тем больше вероятность ошибки сервера. \n");
+    
+    var requestQuantity = [
+      [10,'10'],
+      [15,'15'],
+      [20,'20',true],
+ 
+    ];
+    
+    settings.createMultiOpt("requestQuantity", "Количество запрашиваемых данных в одном запросе", requestQuantity, function(v) {
+        service.requestQuantity = v;
+    });
+    
+    var qualityNotAvailableError = "Невозможно открыть видео в "+Resolution[service.Resolution][1]+", используется максимально доступное качество";
 
     function startPage(page) {
-        setPageHeader(page, 'фильмы, сериалы и мультфильмы в HD.');
+        
         var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
             method: 'POST',
             headers: {
-                'User-Agent': 'Android;HD Serials v.1.6.3;en-US;motorola DROIDX;SDK 10;v.2.3.3(REL)'
+                'User-Agent': USER_AGENT
             },
             args: {
                 id: 'common-categories'
@@ -81,14 +114,14 @@
                 icon: logo
             });
         }
+        setPageHeader(page, 'фильмы, сериалы и мультфильмы в HD.');
     }
     plugin.addURI(PREFIX + ":news:(.*)", function(page, id) {
         var counter = 0;
-        setPageHeader(page, 'Сериалы HD новинки');
         var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
             method: 'POST',
             headers: {
-                'User-Agent': 'Android;HD Serials v.1.6.3;en-US;motorola DROIDX;SDK 10;v.2.3.3(REL)'
+                'User-Agent': USER_AGENT
             },
             args: {
                 id: id
@@ -102,14 +135,14 @@
             });
             counter++;
         }
+        setPageHeader(page, 'Сериалы HD новинки');
     });
     // Shows genres of the category jump to sub-categories
     plugin.addURI(PREFIX + ":common-categories:(.*):(.*)", function(page, id, title) {
-        setPageHeader(page, unescape(title));
         var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
             method: 'POST',
             headers: {
-                'User-Agent': 'Android;HD Serials v.1.6.3;en-US;motorola DROIDX;SDK 10;v.2.3.3(REL)'
+                'User-Agent': USER_AGENT
             },
             args: {
                 id: 'sub-categories',
@@ -123,29 +156,87 @@
                 icon: logo
             });
         }
+        setPageHeader(page, unescape(title));
     });
     // Shows sub-categories jump to filter-videos
     plugin.addURI(PREFIX + ":sub-categories:(.*):(.*):(.*)", function(page, category_id, title, video_count) {
         var offset = 0;
         var counter = 0;
         var anchor = 0;
-        setPageHeader(page, unescape(title));
+	var lastRequest = 0,
+	    requestFinished = true,
+	    requestNumber=0;
+	
+	
+	//trying to implement a delay function to prevent server overload when fast scrolling
+	//returns value of time before next request can be made, or zero if the request can be made immediately
+	function countDelay(delay) {
+	  //showtime.print('Getting difference between:'+lastRequest+" and "+showtime.time());
+	  var timeDiff = getTimeDifference(lastRequest,showtime.time())*1000;
+	  //showtime.print("time sinse last call:"+timeDiff);
+	  if(timeDiff<delay) {
+	    //wait for the delay time to end
+	    return delay-timeDiff;
+	  }
+	  else {
+	   return 0; 
+	  }
+	  
+	}
+
 
         function loader() {
-            if (parseInt(video_count, 10) <= counter) return false;
-            var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
-                method: 'POST',
-                headers: {
-                    'User-Agent': 'Android;HD Serials v.1.6.3;en-US;motorola DROIDX;SDK 10;v.2.3.3(REL)'
-                },
-                args: {
-                    id: 'filter-videos',
-                    category: category_id,
-                    fresh: 1,
-                    start: offset,
-                    limit: 25
-                }
-            }));
+	  if(!requestFinished) {
+	    //showtime.print("Request not finished yet, exiting");
+	    return false;
+	  }
+	  //showtime.print(video_count+" "+counter);
+            if (parseInt(video_count) <= counter) {
+	      if(service.showEndOfDirMessage && requestNumber>2) {
+		showtime.notify("Достигнут конец директории",2);
+	      }
+	      return false;
+	    }
+	    var JSON;
+	      
+	      var delay = countDelay(service.requestMinDelay);
+	      
+	      var loadJSON = function() {
+		try {
+		lastRequest = showtime.time();
+		requestFinished = false;
+		//showtime.print("Time to make some requests now!");
+		var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
+		    method: 'POST',
+		    headers: {
+			'User-Agent': USER_AGENT
+		    },
+		    args: {
+			id: 'filter-videos',
+			category: category_id,
+			fresh: 1,
+			start: offset,
+			limit: service.requestQuantity
+		    }
+		}));
+		requestFinished = true;
+		requestNumber++;
+		//showtime.print("Request finished!. Got "+JSON.data.length);
+		return JSON;
+		}
+		catch(err) {
+		 showtime.notify("Подгрузка контента не удалась. Возможно, сервер не ответил вовремя.",5);
+		 return false;
+		}
+	      };
+	      
+		  //showtime.print("Let's wait "+delay+" msec before making a request!");
+		  showtime.sleep(delay);
+		  JSON = loadJSON();
+		  
+		  if(!JSON) return false;
+	      
+	      
             for (var i in JSON.data) {
                 page.appendItem(PREFIX + ':' + JSON.id + ':' + JSON.data[i].id + ':' + escape(JSON.data[i].title_ru + (JSON.data[i].season ? " " + showtime.entityDecode(JSON.data[i].season) : "")), "video", {
                     title: showtime.entityDecode(unescape(JSON.data[i].title_ru)) + (JSON.data[i].title_en ? " / " + showtime.entityDecode(JSON.data[i].title_en) : "") + (JSON.data[i].season ? " " + showtime.entityDecode(JSON.data[i].season) : ""),
@@ -154,20 +245,21 @@
                 });
                 counter++;
             }
-            offset += 25;
+            offset += JSON.data.length;
             return true;
         }
         loader();
-        page.loading = false;
+        setPageHeader(page, unescape(title));
         page.paginator = loader;
     });
+    
+    
     plugin.addURI(PREFIX + ":filter-videos:(.*):(.*)", function(page, id, title) {
         var i, genres, actors, directors;
-        setPageHeader(page, unescape(title));
         var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
             method: 'POST',
             headers: {
-                'User-Agent': 'Android;HD Serials v.1.6.3;en-US;motorola DROIDX;SDK 10;v.2.3.3(REL)'
+                'User-Agent': USER_AGENT
             },
             args: {
                 id: 'video',
@@ -209,9 +301,12 @@
             });
             //item.bindVideoMetadata({title: JSON.data.info.title_en, season: 2, episode: parseInt(i)+1,  year: parseInt(JSON.data.info.year)})
         }
+        setPageHeader(page, unescape(title));
     });
     // Play links
     plugin.addURI(PREFIX + ":video:(.*):(.*)", function(page, url, title) {
+	//no loading circle was present, forcing
+	page.loading = true;
         var video = get_video_link(unescape(url));
         if (showtime.probe(video).result === 0) {
             page.type = "video";
@@ -269,7 +364,6 @@
             }
         } else {
             v = url.match("video\/(.*?)\/iframe")[1];
-            //var JSON = showtime.JSONDecode(showtime.httpReq('http://moonwalk.cc/sessions/create', {
             var JSON = showtime.JSONDecode(showtime.httpReq('http://moonwalk.cc/sessions/create_session', {
                 debug: true,
                 postdata: {
@@ -278,23 +372,39 @@
             }));
             result_url = 'hls:' + JSON.manifest_m3u8;
             var m = /.*index.m3u8.*/g.execAll(showtime.httpReq(JSON.manifest_m3u8));
-            showtime.print('service.Resolution' + service.Resolution);
             switch (service.Resolution) {
             case '0':
                 result_url = 'hls:' + JSON.manifest_m3u8;
                 break;
             case '1':
+	      //not each and every video contains 720p link
+	      //as a workaround, play best quality available
+	      //or m[2] will be undefined. This will give us 403 error on playback.
+	      if(m[2]) {
+		//well, same here
+		//except I've never seen such videos. Just in case...
                 result_url = 'hls:' + JSON.manifest_m3u8.split('mbr')[0] + m[2];
+	      }
+	      else {
+		result_url = 'hls:' + JSON.manifest_m3u8.split('mbr')[0] + m[m.length-1];
+		showtime.notify(qualityNotAvailableError,5);
+	      }
                 break;
             case '2':
+	      if(m[1]) {
                 result_url = 'hls:' + JSON.manifest_m3u8.split('mbr')[0] + m[1];
+	      }
+	      else {
+		result_url = 'hls:' + JSON.manifest_m3u8.split('mbr')[0] + m[m.length-1];
+	      }
                 break;
             case '3':
+	      //let's assume that at least one item is available
                 result_url = 'hls:' + JSON.manifest_m3u8.split('mbr')[0] + m[0];
                 break;
             }
         }
-        showtime.trace("Video Link: " + result_url);
+        showtime.print("Video Link: " + result_url);
         return result_url;
     }
 
@@ -326,19 +436,18 @@
     plugin.addSearcher(PREFIX + " - Videos", plugin.path + "logo.png", function(page, query) {
         try {
             var offset = 0;
-            setPageHeader(page, query);
             var loader = function loader() {
                     var JSON = showtime.JSONDecode(showtime.httpReq(BASE_URL + '/backend/model.php', {
                         method: 'POST',
                         headers: {
-                            'User-Agent': 'Android;HD Serials v.1.6.3;en-US;motorola DROIDX;SDK 10;v.2.3.3(REL)'
+                            'User-Agent': USER_AGENT
                         },
                         args: {
                             id: 'filter-videos',
                             category: 0,
                             search: query,
                             start: offset,
-                            limit: 25
+                            limit: 20
                         }
                     }));
                     for (var i in JSON.data) {
@@ -349,14 +458,19 @@
                         });
                         page.entries++;
                     }
-                    offset += 25;
+                    offset += 20;
                     return !JSON.endOfData;
                 };
+	    setPageHeader(page, query);
             loader();
-            page.loading = false;
             page.paginator = loader;
         } catch (err) {
             showtime.trace('HDSerials.ru - Ошибка поиска: ' + err);
         }
     });
+    
+    function getTimeDifference(startUnix,endUnix) {
+  return endUnix-startUnix; //in milliseconds
+}
+    
 })(this);
